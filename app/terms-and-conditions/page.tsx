64ed758ca2e65b0142583c4a2e38f17267da5328
Juan Pablo Vargas <jupavar@gmail.com>
2023-10-05 10:43:40 -0300

    Jupadev update (#6)
    * Create page.tsx
    
    * add terms and condition page
    
    * update terms

---
+++ app/terms-and-conditions/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../partials/Header';
+
+function TermsAndConditions() {
+  
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      <Header />
+      <main className="grow">
+        Terms and conditions
+      </main>
+    </div>
+  );
+}
+
+export default TermsAndConditions;
