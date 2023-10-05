5ffd176a177e46dc88e733d3057a71ab37202245
Juan Pablo Vargas <jupavar@gmail.com>
2023-10-05 10:41:11 -0300

    add terms and condition page

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,1 +1,16 @@
-<div>terms and contions</div>
+import React, { useEffect } from 'react';
+import Header from '../partials/Header';
+
+function TermsAndConditions() {
+  
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      <Header />
+      <main className="grow">
+        Terms and conditions
+      </main>
+    </div>
+  );
+}
+
+export default TermsAndConditions;
