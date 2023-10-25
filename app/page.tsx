056e2648731745ddc6b11230d5c8a47f9385c3dd
Juan Pablo Vargas <jupavar@gmail.com>
2023-10-25 11:16:32 -0300

    styles: remove feature zigzag

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Header from './partials/Header';
 import PageIllustration from './partials/PageIllustration';
 import HeroHome from './partials/HeroHome';
 import FeaturesBlocks from './partials/FeaturesBlocks';
-import FeaturesZigZag from './partials/FeaturesZigzag';
 import Testimonials from './partials/Testimonials';
 import Newsletter from './partials/Newsletter';
 import Footer from './partials/Footer';
@@ -30,7 +29,6 @@ function Home() {
         </div>
         <HeroHome />
         <FeaturesBlocks />
-        <FeaturesZigZag />
         <Testimonials />
         <Newsletter />
       </main>
